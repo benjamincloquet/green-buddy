8876c61e17bf4dd705ed3f48121c257dd446c662
Ben <benjamin.cloquet@gmail.com>
2020-11-16 18:02:32 +0100

    Password Hashing

--- database.js
+++ database.js
@@ -1,6 +1,4 @@
-const mongoose = require('mongoose');
-
-mongoose
+require('mongoose')
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,44 @@
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/User');
+
+const hashPassword = (password) => new Promise((resolve, reject) => {
+  bcrypt.genSalt(process.env.BCRYPT_SALT_ROUNDS || 10, (saltErr, salt) => {
+    if (saltErr) {
+      reject(saltErr);
+    }
+    bcrypt.hash(password, salt, (hashErr, hashedPassword) => {
+      if (hashErr) {
+        reject(hashErr);
+      }
+      reject();
+    });
+  });
+});
+
+const registerUser = (user) => new Promise((resolve, reject) => {
+  hashPassword(user.password).then((encryptedPassword) => {
+    new User({ ...user, password: encryptedPassword })
+      .save()
+      .then(() => {
+        resolve();
+      })
+      .catch(() => {
+        reject();
+      });
+  }).catch(() => {
+    reject();
+  });
+});
+
 module.exports = (router) => {
   router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('Hello');
+    registerUser(req.body)
+      .then(() => {
+        res.status(201).json();
+      })
+      .catch(() => {
+        res.status(503).json({ error: "Couldn't register new user" });
+      });
   });
 };
