2c72c5bad11c0261183d29fc658df0f1158bd4c3
Ben <benjamin.cloquet@gmail.com>
2020-11-18 17:46:57 +0100

    Improve cart product add and quantity change

--- routes/cart.js
+++ routes/cart.js
@@ -4,29 +4,48 @@ const connectedToDatabase = require('./middleware/connectedToDatabase');
 
 const CartProduct = require('../models/CartProduct');
 
+const createCartProduct = async (req) => {
+  const quantity = Math.max(req.body.quantity, 0);
+  return new CartProduct({ ...req.body, quantity, userId: req.user.id }).save();
+};
+
+const getCartProduct = async (req) => {
+  const cartProducts = await CartProduct.find({ userId: req.user.id });
+  const { productId } = req.body;
+  return cartProducts.find((cartProduct) => cartProduct.productId.equals(productId));
+};
+
+const changeCartProductQuantity = async (cartProduct, quantity) => {
+  if (quantity > 0) {
+    const updatedCartProduct = cartProduct;
+    updatedCartProduct.quantity = quantity;
+    return cartProduct.save();
+  }
+  await CartProduct.deleteOne({ id: cartProduct.id });
+  return null;
+};
+
 module.exports = (router) => {
   router.get('/cart', authenticated, (req, res) => {
     res.send(req.user.cart);
   });
 
-  router.post('/cart', connectedToDatabase, authenticated, productExists, (req, res) => {
-    CartProduct.find({ userId: req.user.id }, (err, cartProducts) => {
-      if (err) {
-        res.status(503).json({ error: "Couldn't add products to the cart" });
-      } else {
-        const { productId, quantity } = req.body;
-        const existingCartProduct = cartProducts.find((cartProduct) => cartProduct.productId.equals(productId));
-        if (existingCartProduct) {
-          existingCartProduct.quantity += quantity;
-          existingCartProduct.save().then((savedCartProduct) => {
-            res.status(201).json(savedCartProduct);
-          });
-        } else {
-          new CartProduct({ ...req.body, userId: req.user.id }).save().then((newCartProduct) => {
-            res.status(201).json(newCartProduct);
-          });
-        }
-      }
-    });
+  router.post('/cart', connectedToDatabase, authenticated, productExists, async (req, res) => {
+    try {
+      const cartProduct = await getCartProduct(req);
+      const updatedCartProduct = cartProduct ? await changeCartProductQuantity(cartProduct, req.body.quantity) : await createCartProduct(req);
+      res.status(201).json(updatedCartProduct);
+    } catch (err) {
+      res.status(503).json({ error: "Couldn't add products to the cart" });
+    }
+  });
+
+  router.delete('/cart', connectedToDatabase, authenticated, async (req, res) => {
+    try {
+      await CartProduct.deleteOne({ id: req.body.cartProductId });
+      res.sendStatus(200);
+    } catch (err) {
+      res.status(503).json({ error: "Couldn't delete product from the cart" });
+    }
   });
 };
